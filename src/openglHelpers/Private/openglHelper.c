ce4eae32b49a38f109fb4e1d8f01828670365795
darkus <jmjoymallik@gmail.com>
2024-11-29 10:56:23 +0530

    Update core profile to 4.5

--- src/openglHelpers/Private/openglHelper.c
+++ src/openglHelpers/Private/openglHelper.c
@@ -4,14 +4,18 @@
 
 #include "openglHelper.h"
 #include "stdio.h"
+#include <stdio.h>
+
 
 GLFWwindow *setupContext() {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow *window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
+
 
     if (window == NULL) {
         printf("Failed to create GLFW window\n");
@@ -19,7 +23,6 @@ GLFWwindow *setupContext() {
         return NULL;
     }
     glfwMakeContextCurrent(window);
-
     // Setup GLAD. Glad manages function pointers to opengl functions, since opengl is just a spec
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
         printf("Failed to initialize GLAD\n");
